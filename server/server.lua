39b33274527dea67712f3a1478212abf641f0ca1
Jaysigx <50681690+Jaysigx@users.noreply.github.com>
2023-12-16 19:33:59 -0500

    Update server.lua
    support client

--- server/server.lua
+++ server/server.lua
@@ -8,23 +8,17 @@ end)
 
 RegisterServerEvent('PE-Bridge:AdjustBridgeHeight')
 AddEventHandler('PE-Bridge:AdjustBridgeHeight', function(amount)
-    local source = source
-    -- Calculate the new height based on the 'amount'
-    local currentHeight = bridgeHeight or initialBridgePosition.z 
+    local currentHeight = targetBridgeHeight or initialBridgePosition.z 
     local newHeight = currentHeight + amount
-
-    -- Ensure the newHeight is within the valid range (minBridgeHeight to maxBridgeHeight)
     newHeight = math.min(math.max(newHeight, minBridgeHeight), maxBridgeHeight)
+    targetBridgeHeight = newHeight
 
-    -- Trigger the client event to update bridge height for all players
     TriggerClientEvent('PE-Bridge:SetBridgeHeight', -1, newHeight)
 end)
 
 -- Client-side event to receive and update bridge height
 RegisterNetEvent('PE-Bridge:SetBridgeHeight')
 AddEventHandler('PE-Bridge:SetBridgeHeight', function(newHeight)
-    -- Update the bridge height in the client-side logic
-    -- For example, if you're handling the bridge visuals on the client side, update it here
 end)
 
 -- Function to adjust the bridge height and trigger the server event
@@ -41,9 +35,7 @@ RegisterCommand("lowerBridge", function()
     adjustBridgeHeight(-20.0)
 end, false)
 
--- Example: Triggering the bridge spawn when a player joins
-AddEventHandler('playerSpawned', function()
-    TriggerClientEvent('PE-Bridge:spawnBridge', source)
-end)
+
+
 
 
