add6e4bd759922658f9966bf1ee70b28ddaad8be
Jaysigx <50681690+Jaysigx@users.noreply.github.com>
2023-12-18 05:39:35 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
 Config = {}
+Config.Bridge1Position = vector3(353.3317, -2315.838, 6.9134)
 
-
-Config.BridgePosition = vector3(353.3317, -2315.838, 6.8615)
+Config.Bridge2Position = vector3(219.5085, -2319.442, 5.0135)
