842599c0872bf5e47069717a39ec482e154198fc
Jaysigx <50681690+Jaysigx@users.noreply.github.com>
2023-12-17 20:17:48 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,22 +1,26 @@
 fx_version 'cerulean'
 game 'gta5'
 
-name 'Jaysigx-Bridge'
-description 'Bridge go upy and downy'
+name 'Jaysigx-DrawBridge'
+description 'Drawbridge script for FiveM'
 author 'Jaysigx'
 
+
+files {
+    'stream/**/car_drawbridge.ydr',
+}
 client_script {
     'client/client.lua',
     'client/gates.lua'
 }
-server_scripts {
-    'server/*.lua', 
+
+server_script {
+    'server/server.lua'
 }
 
 shared_script {
     'config.lua' -- Include the config file here as well if needed
 }
 
-data_file 'DLC_ITYP_REQUEST' 'stream/*.ytyp' 
+this_is_a_map 'yes'
 
-map 'map.lua' 
