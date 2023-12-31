a21291a72fdbf95f1330692f25204f5b7cd4350f
Jaysigx <50681690+Jaysigx@users.noreply.github.com>
2023-12-31 00:20:16 -0500

    Update cl_spawnbridge.lua
    optimized loading. fixed only 1 bridge spawning

--- client/cl_spawnbridge.lua
+++ client/cl_spawnbridge.lua
@@ -1,29 +1,26 @@
 local bridgeObjects = {}
 local initialBridgePositions = {Config.Bridge1Position, Config.Bridge2Position}
 local models = {joaat('car_drawbridge'), joaat('train_drawbridge')}
-local isBridgeCreated = {false, false}
-local bridgeEntities = {nil, nil}
+local isBridgeCreated = {car = false, train = false}
+local bridgeEntities = {car = nil, train = nil}
 
--- Function to load models
-function LoadModel(modelIndex)
-    local model = models[modelIndex]
-
-    if not HasModelLoaded(model) then
-        RequestModel(model)
-        Citizen.Wait(100)
-        while not HasModelLoaded(model) do
-            Citizen.Wait(100)
+function LoadModels()
+    for _, model in ipairs(models) do
+        if not HasModelLoaded(model) then
+            RequestModel(model)
+            while not HasModelLoaded(model) do
+                Citizen.Wait(100)
+            end
+            print("Model loaded successfully: " .. model)
         end
-        print("Model loaded successfully: " .. model)
     end
 end
 
--- Function to create bridge objects
 function CreateBridgeObject(index)
     local model = models[index]
     local initialBridgePosition = initialBridgePositions[index]
 
-    LoadModel(index)
+    LoadModels()
 
     if not isBridgeCreated[index] or not DoesEntityExist(bridgeEntities[index]) then
         if bridgeEntities[index] and DoesEntityExist(bridgeEntities[index]) then
@@ -33,7 +30,7 @@ function CreateBridgeObject(index)
         bridgeEntities[index] = CreateObject(model, initialBridgePosition.x, initialBridgePosition.y, initialBridgePosition.z, true, true, false)
 
         if bridgeEntities[index] and bridgeEntities[index] ~= 0 then
-            SetEntityLodDist(bridgeEntities[index], 500)
+            SetEntityLodDist(bridgeEntities[index], 250)
             SetEntityAsMissionEntity(bridgeEntities[index], true, true)
             FreezeEntityPosition(bridgeEntities[index], true)
             SetEntityInvincible(bridgeEntities[index], true)
@@ -42,12 +39,10 @@ function CreateBridgeObject(index)
         else
             print("Failed to create Bridge object. Model may be invalid or position is obstructed.")
         end
-    else
-        print("Bridge object already exists.")
     end
 end
 
--- Function to check and update bridge objects
+-- Function to create or update bridge objects
 function CreateOrUpdateBridgeObjects()
     for index, _ in ipairs(initialBridgePositions) do
         if not isBridgeCreated[index] or not DoesEntityExist(bridgeEntities[index]) then
@@ -57,42 +52,28 @@ function CreateOrUpdateBridgeObjects()
     end
 end
 
--- Thread to manage initial bridge objects creation/update
-Citizen.CreateThread(function()
-    for index, _ in ipairs(initialBridgePositions) do
-        CreateOrUpdateBridgeObjects()
-    end
-
-    local areBridgesStillLoading = true
-    while areBridgesStillLoading do
-        Citizen.Wait(5000)
-
-        for index, model in ipairs(models) do
-            if HasModelLoaded(model) then
-                areBridgesStillLoading = false
-            else
-                print("Bridge " .. index .. " Model is still loading...")
-            end
-        end
-    end
-end)
 
 -- Function to count existing bridges near a position
-function CountExistingBridgesNearPosition(position)
-    local count = 0
+function CountExistingCarAndTrainBridgesNearPosition(position)
+    local countCar = 0
+    local countTrain = 0
     for _, bridgeEntity in pairs(bridgeEntities) do
         if bridgeEntity and DoesEntityExist(bridgeEntity) then
             local distance = #(GetEntityCoords(bridgeEntity) - position)
             if distance < 250.0 then
-                count = count + 1
+                if bridgeEntity == bridgeEntities.car then
+                    countCar = countCar + 1
+                elseif bridgeEntity == bridgeEntities.train then
+                    countTrain = countTrain + 1
+                end
             end
         end
     end
-    return count
+    return countCar, countTrain
 end
 
--- Function to spawn bridge if not exists near player
-function SpawnBridgeIfNotExists(index)
+-- Function to manage bridge spawning based on player proximity
+function SpawnCarOrTrainBridgeIfNotExists(index)
     Citizen.CreateThread(function()
         while true do
             Citizen.Wait(500)
@@ -101,17 +82,25 @@ function SpawnBridgeIfNotExists(index)
             local distance = #(playerCoords - initialBridgePositions[index])
 
             if distance < 250.0 then
-                local existingBridgesNearPlayer = CountExistingBridgesNearPosition(initialBridgePositions[index])
+                local existingCar, existingTrain = CountExistingCarAndTrainBridgesNearPosition(initialBridgePositions[index])
 
-                if existingBridgesNearPlayer < 1 then
-                    if not isBridgeCreated[index] then
-                        CreateBridgeObject(index)
-                    end
+                if existingCar < 1 and not isBridgeCreated.car then
+                    CreateBridgeObject(1)
                 end
-            elseif isBridgeCreated[index] then
-                DeleteEntity(bridgeEntities[index])
-                bridgeEntities[index] = nil
-                isBridgeCreated[index] = false
+
+                if existingTrain < 1 and not isBridgeCreated.train then
+                    CreateBridgeObject(2)
+                end
+            else
+                if index == 1 and isBridgeCreated.car then
+                    DeleteEntity(bridgeEntities.car)
+                    bridgeEntities.car = nil
+                    isBridgeCreated.car = false
+                elseif index == 2 and isBridgeCreated.train then
+                    DeleteEntity(bridgeEntities.train)
+                    bridgeEntities.train = nil
+                    isBridgeCreated.train = false
+                end
             end
         end
     end)
@@ -120,10 +109,9 @@ end
 -- Thread to check and spawn bridge objects near players
 Citizen.CreateThread(function()
     for index, _ in ipairs(initialBridgePositions) do
-        SpawnBridgeIfNotExists(index)
+        SpawnCarOrTrainBridgeIfNotExists(index)
     end
 end)
-
 
 -- Event handler for resource start - creates bridges on resource start
 AddEventHandler('onResourceStart', function()
@@ -137,7 +125,6 @@ RegisterNetEvent('bridge:spawnBridge')
 AddEventHandler('bridge:spawnBridge', function()
     for index, _ in ipairs(initialBridgePositions) do
         CreateBridgeObject(index)
-        CreateOrUpdateBridgeObjects(index)
     end
 end)
 
